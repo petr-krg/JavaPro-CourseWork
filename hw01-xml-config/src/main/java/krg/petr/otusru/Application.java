eca8c065b0b46a309db7c6af63044ab20fbb7d49
petr.krg <petr.krg@mail.ru>
2023-10-11 23:21:49 +0600

    Первая сдача на проверку.

---
+++ hw01-xml-config/src/main/java/krg/petr/otusru/Application.java
@@ -0,0 +1,15 @@
+package krg.petr.otusru;
+
+import krg.petr.otusru.service.TestRunnerService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+
+        //Прописать бины в spring-context.xml и создать контекст на его основе
+        ApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
+        var testRunnerService = context.getBean(TestRunnerService.class);
+        testRunnerService.run();
+    }
+}
